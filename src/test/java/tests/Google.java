a66ee881b5ee8ee0d374ba41faf5488918c64e7d
Ai2rgan <kuralergeshov.ke@gmail.com>
2023-09-27 13:56:39 -0500

    added google images test

--- src/test/java/tests/Google.java
+++ src/test/java/tests/Google.java
@@ -16,5 +16,12 @@ public class Google {
         Driver.getDriver().findElement(By.name("q")).sendKeys(testData + Keys.ENTER);
         String title = Driver.getDriver().getTitle();
         Assert.assertTrue("Title failed", title.contains(testData));
+        Driver.quit();
     }
-}
+
+    @Test
+    public void googleImage(){
+        Driver.getDriver().get("https://google.com");
+        System.out.println("Checking some images");
+
+}}
