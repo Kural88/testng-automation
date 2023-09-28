8139c935445d909985c396a3cce69d68bba769c0
Ai2rgan <kuralergeshov.ke@gmail.com>
2023-09-28 15:09:06 -0500

    removing search test configurations

--- src/test/java/tests/Google.java
+++ src/test/java/tests/Google.java
@@ -13,15 +13,14 @@ public class Google {
         Faker faker = new Faker();
         String testData = faker.lorem().word();
         Driver.getDriver().get("https://google.com");
-        Driver.getDriver().findElement(By.name("q")).sendKeys(testData + Keys.ENTER);
-        String title = Driver.getDriver().getTitle();
-        Assert.assertTrue("Title failed", title.contains(testData));
-        Driver.quit();
+
+
     }
 
     @Test
     public void googleImage(){
         Driver.getDriver().get("https://google.com");
         System.out.println("Checking some images");
+        Driver.quit();
 
 }}
